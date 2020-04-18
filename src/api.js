5fbceb7bd44113d4d4ff13e960024b5250ed205b
Jinyoung Kim <jinyoung777@gmail.com>
2020-04-18 13:27:40 +1200

    reset changes

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ const serverless = require('serverless-http');
 const app = express();
 
 var corsOptions = {
-  origin: 'https://word-jason.netlify.app',
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST, DELETE, OPTIONS',
   preflightContinue: true,
   optionsSuccessStatus: 204
@@ -21,7 +21,7 @@ app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Words API...' }));
 
 app.use(cors(corsOptions));
-app.options('*', cors())
+//app.options('*', cors())
 
 // Define Routes
 // app.use('/api/users', require('./routes/users'));
